a205ed73d2f9867f452c63fab77792180ec89e13
Nico Landu <74741743+nicolandu@users.noreply.github.com>
2024-04-03 22:55:07 -0400

    First commit

---
+++ config.fish
@@ -0,0 +1,38 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g theme_color_scheme gruvbox
+    set -g theme_nerd_fonts yes
+
+    abbr --add y xsel -b # [Y]ank to clipboard
+    abbr --add p xsel -ob # [P]aste from clipboard
+    abbr --add nv nvim
+    abbr --add lg lazygit
+    abbr --add tp trash-put
+    abbr --add tm tmux
+    abbr --add tmnw tmux new-win
+    
+    function multicd
+        echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+    end
+    abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+    set -g fish_key_bindings fish_vi_key_bindings
+    # Emulates vim's cursor shape behavior
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert line
+    # Set the replace mode cursors to an underscore
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    # Set the external cursor to a line. The external cursor appears when a command is started.
+    # The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+    set fish_cursor_external line
+    # The following variable can be used to configure cursor shape in
+    # visual mode, but due to fish_cursor_default, is redundant here
+    set fish_cursor_visual block
+
+    # C-F autocomplete
+    bind -M visual \cF end-of-line
+    bind -M insert \cF end-of-line
+end
